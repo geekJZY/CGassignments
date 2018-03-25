fee7ca8ebe75281e54007278e922b174c9615753
geekJZY <jiangziyu@tamu.edu>
2018-03-25 15:58:49 -0500

    project06 first parallel projecting finished

--- Project06/main.cpp
+++ Project06/main.cpp
@@ -13,22 +13,28 @@ using namespace cv;
 class Quadrics{
 private:
     Vec4f ColorH0, ColorH1, ColorH2;
+    Mat textureMap;
+public:
     enum SphereType{
         TEXTURE,
+        SOLID_TEXTURE,
+        SOLID_TEXTURE_PER,
         HOMO
     };
     SphereType sphereType;
-    Mat textureMap;
-public:
     int a02,a12,a22,a21,a00;
     double s0, s1, s2;
+    float x_solid, y_solid;
     Vec3d n0, n1, n2;
     Vec4f  ColorH3;
     Vec3d pc;
+    Vec3d planeA, planeY, planeX, planeC, projPoint;
     bool checkIn(Vec3d pos);
     double rayTracer(Vec3d pe, Vec3d npe);
     Quadrics(int a02_, int a12_, int a22_, int a21_, int a00_, double s0_, double s1_, double s2_, Vec3d pc_, Vec4f ColorH0_, Vec4f ColorH1_, Vec4f ColorH2_, Vec4f ColorH3_);
     Quadrics(int a02_, int a12_, int a22_, int a21_, int a00_, double s0_, double s1_, double s2_, Vec3d pc_, Mat textureMap_, Vec4f ColorH3_);
+    Quadrics(int a02_, int a12_, int a22_, int a21_, int a00_, double s0_, double s1_, double s2_, Vec3d pc_, Mat textureMap_, Vec3d planeA_, Vec3d planeX_, Vec3d planeC_, Vec3d projPoint_, float x_solid, float y_solid, Vec4f ColorH3_, SphereType _sphereType = SOLID_TEXTURE);
+
     vector<Vec4f> TexColorDeter(Vec3d Ph);
     Vec3d nH(Vec3d Ph);
 };
@@ -77,6 +83,31 @@ Quadrics::Quadrics(int a02_, int a12_, int a22_, int a21_, int a00_, double s0_,
     sphereType = TEXTURE;
 }
 
+Quadrics::Quadrics(int a02_, int a12_, int a22_, int a21_, int a00_, double s0_, double s1_, double s2_, Vec3d pc_, Mat textureMap_, Vec3d planeA_, Vec3d planeX_, Vec3d planeC_, Vec3d projPoint_, float x_solid_, float y_solid_, Vec4f ColorH3_, SphereType _sphereType){
+    a02 = a02_;
+    a12 = a12_;
+    a22 = a22_;
+    a21 = a21_;
+    a00 = a00_;
+    s0 = s0_;
+    s1 = s1_;
+    s2 = s2_;
+    pc = pc_;
+    textureMap = textureMap_;
+    ColorH3 = ColorH3_;
+    n0 = Vec3d(1,0,0);
+    n1 = Vec3d(0,1,0);
+    n2 = Vec3d(0,0,1);
+    sphereType = _sphereType;
+    planeA = planeA_;
+    planeX = planeX_;
+    planeY = planeA.cross(planeX);
+    planeC = planeC_;
+    x_solid = x_solid_;
+    y_solid = y_solid_;
+    projPoint = projPoint_;
+}
+
 
 bool Quadrics::checkIn(Vec3d pos){
     double F = pow((pos-pc).dot(n0)/s0,2)*a02+pow((pos-pc).dot(n1)/s1,2)*a12+pow((pos-pc).dot(n2)/s2,2)*a22+a00;
@@ -109,7 +140,7 @@ vector<Vec4f> Quadrics::TexColorDeter(Vec3d Ph){
         texColors[2] = ColorH2;
         return texColors;
     }
-    else{
+    else if(sphereType == TEXTURE){
         Vec3d npe;
         npe = (Ph-pc)/norm(Ph-pc);
         double x,y,z,theta,phi;
@@ -122,6 +153,68 @@ vector<Vec4f> Quadrics::TexColorDeter(Vec3d Ph){
         if(x<0) theta = 2*PI - theta;
         X = max(min(int(round((theta/(2*PI))*textureMap.cols)),textureMap.cols),0);
         Y = max(min(int(round((phi/(PI))*textureMap.rows)),textureMap.rows),0);
+
+        texColors[1][0] = textureMap.at<Vec3b>(Y, X)[0];
+        texColors[1][1] = textureMap.at<Vec3b>(Y, X)[1];
+        texColors[1][2] = textureMap.at<Vec3b>(Y, X)[2];
+        texColors[1][3] = 255;
+
+        for(int i = 0; i < 3; i ++){
+            texColors[0][i] = texColors[1][i]/5;
+            texColors[2][i] = min(texColors[1][i], float(255.0));
+            texColors[2][i] *= 10;
+        }
+        texColors[0][3] = 255;
+        texColors[2][3] = 2550;
+        return texColors;
+    }
+    else if(sphereType == SOLID_TEXTURE){
+        Vec3d vpc;
+        vpc = Ph-planeC;
+        double x,y;
+        int X,Y;
+        x = vpc.dot(planeX);
+        y = vpc.dot(planeY);
+        X = x / x_solid * textureMap.cols;
+        Y = y / y_solid * textureMap.rows;
+        if(X < 0 || Y < 0 || X >= textureMap.cols || Y >= textureMap.rows){
+            for(int i = 0; i < 3; i ++) texColors[i] = Vec4f(0,0,0,0);
+            return texColors;
+        }
+
+        texColors[1][0] = textureMap.at<Vec3b>(Y, X)[0];
+        texColors[1][1] = textureMap.at<Vec3b>(Y, X)[1];
+        texColors[1][2] = textureMap.at<Vec3b>(Y, X)[2];
+        texColors[1][3] = 255;
+
+        for(int i = 0; i < 3; i ++){
+            texColors[0][i] = texColors[1][i]/5;
+            texColors[2][i] = min(texColors[1][i], float(255.0));
+            texColors[2][i] *= 10;
+        }
+        texColors[0][3] = 255;
+        texColors[2][3] = 2550;
+        return texColors;
+    }
+    else if(sphereType == SOLID_TEXTURE_PER){
+        Vec3d vph, pI;
+        vph = Ph-projPoint;
+        double t,x,y;
+        int X,Y;
+        t = planeA.dot(planeC - projPoint)/(planeA.dot(vph));
+        if(t < 0 || t > 1){
+            for(int i = 0; i < 3; i ++) texColors[i] = Vec4f(0,0,0,0);
+            return texColors;
+        }
+        pI = projPoint + t * vph - planeC;
+        x = pI.dot(planeX);
+        y = pI.dot(planeY);
+        X = x / x_solid * textureMap.cols;
+        Y = y / y_solid * textureMap.rows;
+        if(X < 0 || Y < 0 || X >= textureMap.cols || Y >= textureMap.rows){
+            for(int i = 0; i < 3; i ++) texColors[i] = Vec4f(0,0,0,0);
+            return texColors;
+        }
 
         texColors[1][0] = textureMap.at<Vec3b>(Y, X)[0];
         texColors[1][1] = textureMap.at<Vec3b>(Y, X)[1];
@@ -506,8 +599,8 @@ int main(int argc, char *argv[]){
     vector<Scalar> color{Scalar(255,224,147),Scalar(93,66,255),Scalar(255,255,255),Scalar(230,180,80)};
     Mat img(Size(640,480),CV_8UC3,color[0]);
     Mat texturePlane = imread("Texture.jpg");
-    Mat sphereTexture = imread("sphereTexture.jpg");
-    Mat sphereTexture2 = imread("sphereTexture2.jpg");
+    Mat sphereTexture = imread("ball_image_small.jpg");
+    Mat sphereTexture2 = imread("ball_image_10_small.jpg");
     Mat normalTexture = imread("normal.jpg");
     double Sx = 16, Sy = 12;
     Vec3d pos;
@@ -519,7 +612,7 @@ int main(int argc, char *argv[]){
 
     //the efficient of background and outline
     double kb = 0.5, k0 = 0.5;
-    colorShadowDeter CSDeter(colorShadowDeter::DOWN_SHADOW);
+    colorShadowDeter CSDeter(colorShadowDeter::SOFT_SHADOW);
     vector<float> shadowFlag;
 
     //Graphics
@@ -527,19 +620,19 @@ int main(int argc, char *argv[]){
     vector<Planes> planes;
     vector<lightSource> lighters;
 
-    spheres.push_back(Quadrics(1,1,1,0,-1,5,5,5,Vec3d(0,0,5),sphereTexture,Vec4f(0,0,0,255)));
-    spheres.push_back(Quadrics(1,1,1,0,-1,3,3,3,Vec3d(0,12,4),sphereTexture2,Vec4f(0,0,0,255)));
+    spheres.push_back(Quadrics(1,1,1,0,-1,5,5,5,Vec3d(0,0,5),sphereTexture,Vec3d(0,-1,0),Vec3d(-1,0,0),Vec3d(6,8,11),Vec3d(0,-12,5),12,12,Vec4f(0,0,0,255),Quadrics::SOLID_TEXTURE));
+    spheres.push_back(Quadrics(1,1,1,0,-1,3,3,3,Vec3d(0,12,4),sphereTexture2,Vec3d(0,-1,0),Vec3d(-1,0,0),Vec3d(4,18,8),Vec3d(0,4,4),8,8,Vec4f(0,0,0,255),Quadrics::SOLID_TEXTURE));
     planes.push_back(Planes(Vec3d(0,0,1),Vec3d(0,0,0),Vec3d(0,1,0),texturePlane,normalTexture,10,10,Vec4f(10,10,10,255)));
 //    planes.push_back(Planes(Vec3d(-1,0,0),Vec3d(12,0,0),Vec4f(50,100,50,255),Vec4f(170,220,170,255),Vec4f(160,210,160,255),Vec4f(10,10,10,255)));
 //    planes.push_back(Planes(Vec3d(0,1,0),Vec3d(0,-9,0),Vec4f(50,50,100,255),Vec4f(170,170,220,255),Vec4f(1800,1800,2300,2550),Vec4f(10,10,10,255)));
 
     //area light
-    lighters.push_back(lightSource(Vec3d(-10,-20,30), Vec3d(0.5773,-0.5773,-0.5773), 0.9,0.5,0.5,Vec4f(60,60,60,60), lightSource::POINTLIGHT));
+    lighters.push_back(lightSource(Vec3d(0,40,50), Vec3d(0.5773,-0.5773,-0.5773), 0.9,0.5,0.5,Vec4f(60,60,60,60), lightSource::POINTLIGHT));
 
     //camera
-    Vec3d pe(40,0,6);
+    Vec3d pe(40,25,6);
     Vec3d vUp(-1,0,1);
-    Vec3d v2(20,0,1);
+    Vec3d v2(20,10,1);
     Vec3d n0Vec(0,-1,0);
     Vec3d n2 = v2/norm(v2);
 
@@ -597,7 +690,7 @@ int main(int argc, char *argv[]){
 
         }
 
-    flip(img, img, 1);
+//    flip(img, img, 1);
     imshow("lena", img);
     imwrite("img.png",img);
     resize(img,img,Size(160,120));
